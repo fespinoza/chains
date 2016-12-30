1a56b4d4f06397ade3ef83aacf2991c702169691
Felipe Espinoza <fespinozacast@gmail.com>
2016-12-30 00:22:24 -0300

    Add implementation for 'Links#Create' action.

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -1,18 +1,4 @@
 # frozen_string_literal: true
 
-process_link = lambda do |_env|
-  data = {
-    link: {
-      id: 2,
-      url: 'http://hanamirb.org/#install',
-      tags: ['ruby', 'web-framework'],
-      created_at: '',
-      updated_at: ''
-    }
-  }
-  json_body = JSON.generate(data)
-  [201, { content_type: 'application/json' }, json_body]
-end
-
-post '/links', to: process_link
+post '/links', to: 'links#create'
 get '/links', to: 'links#index'
