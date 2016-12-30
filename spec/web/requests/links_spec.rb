cbe1e3d50c1b15ba4854ab83601afb8bd9abe589
Felipe Espinoza <fespinozacast@gmail.com>
2016-12-30 12:22:36 -0300

    Ensure presence of tags.

--- spec/web/requests/links_spec.rb
+++ spec/web/requests/links_spec.rb
@@ -38,14 +38,19 @@ class LinksAPITests < Minitest::Test
       }
     end
 
+    def assert_link_json_body(response)
+      json_body = json_response(response)
+      assert_equal json_body[:link][:url], sample_link[:url]
+      assert       json_body[:link][:id]
+      refute_empty json_body[:link][:tags]
+    end
+
     def test_saving_a_new_link
       common_request_preparation
       response = post '/links', link: sample_link
-      json_body = json_response(response)
 
       assert_equal response.status, 201
-      assert_equal json_body[:link][:url], sample_link[:url]
-      assert json_body[:link][:id]
+      assert_link_json_body(response)
     end
 
     def test_unauthenticated_request_to_save_link
