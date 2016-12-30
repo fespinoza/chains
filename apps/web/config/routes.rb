a6634cb49f42f31f580a83367ffd9f732c84e6ee
Felipe Espinoza <fespinozacast@gmail.com>
2016-12-29 22:16:04 -0300

    Add sample route specs.
    Together with lambda based rack apps providing mock implementations.

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -1,5 +1,22 @@
-# Configure your routes here
-# See: http://hanamirb.org/guides/routing/overview/
-#
-# Example:
-# get '/hello', to: ->(env) { [200, {}, ['Hello from Hanami!']] }
+# frozen_string_literal: true
+
+process_link = lambda do |_env|
+  data = {
+    link: {
+      id: 2,
+      url: 'http://hanamirb.org/#install',
+      tags: ['ruby', 'web-framework'],
+      created_at: '',
+      updated_at: ''
+    }
+  }
+  json_body = JSON.generate(data)
+  [201, { content_type: 'application/json' }, json_body]
+end
+
+return_links = lambda do |_env|
+  [200, {}, JSON.generate(links: [])]
+end
+
+post '/links', to: process_link
+get '/links', to: return_links
